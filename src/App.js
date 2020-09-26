c3edc9535247f342d343006194f8a06c48c41f9e
thudsonbu <hudson_thom@bentley.edu>
2020-09-26 10:35:49 -0400

    Fixed bug (hook outside of app component)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import firebase, { auth } from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyBw1Er7O-BNjdktz-CSqWQEkbtpXNvLYWw",
@@ -18,10 +19,13 @@ firebase.initializeApp({
   appId: "1:895604026390:web:379f2ffe3706f652de9937"
 })
 
-const [user] = userAuthState(auth);
-
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -54,7 +58,22 @@ function ChatRoom() {
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
-  
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  return (
+    <div>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/> )}
+      </div>
+    </div>
+  )
 }
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+  return <p>{text}</p>
+}
+
 
 export default App;
